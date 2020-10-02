67b95b16f08ffeee0338495020d5072760c5cbe7
EECS User <user@wsc108-11.eecs.yorku.ca>
2020-10-02 18:33:35 -0400

    Initial commit

---
+++ app/src/main/java/ca/yorku/mcalcpro/MCalcPro_Activity.java
@@ -0,0 +1,16 @@
+package ca.yorku.mcalcpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MCalcPro_Activity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mcalcpro_layout);
+    }
+}
